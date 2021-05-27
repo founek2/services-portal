017d291e4b85240c27101963099967a9e678cadd
Martin Skalický <skalicky.martas@seznam.cz>
2021-05-27 19:18:51 +0200

    Add freeNas + plex

--- src/script.js
+++ src/script.js
@@ -2,101 +2,102 @@
  * Hierarchical timing
  * Add specific delay for CSS3-animation to elements.
  */
-$(document).ready(function() {
-	(function($) {
-		var speed = 2000;
-		var container = $('.display-animation');
-		container.each(function() {
-			var elements = $(this).children();
-			elements.each(function() {
-				console.log('element', this);
-				var elementOffset = $(this).offset();
-				var offset = elementOffset.left * 0.8 + elementOffset.top;
-				var delay = parseFloat(offset / speed).toFixed(2);
-				$(this)
-					.css('-webkit-animation-delay', delay + 's')
-					.css('-o-animation-delay', delay + 's')
-					.css('animation-delay', delay + 's')
-					.addClass('animated');
-			});
-		});
-	})(jQuery);
-	console.log(jQuery);
-	/**
+$(document).ready(function () {
+    (function ($) {
+        var speed = 2000;
+        var container = $(".display-animation");
+        container.each(function () {
+            var elements = $(this).children();
+            elements.each(function () {
+                console.log("element", this);
+                var elementOffset = $(this).offset();
+                var offset = elementOffset.left * 0.8 + elementOffset.top;
+                var delay = parseFloat(offset / speed).toFixed(2);
+                $(this)
+                    .css("-webkit-animation-delay", delay + "s")
+                    .css("-o-animation-delay", delay + "s")
+                    .css("animation-delay", delay + "s")
+                    .addClass("animated");
+            });
+        });
+    })(jQuery);
+    console.log(jQuery);
+    /**
      * Created by Kupletsky Sergey on 04.09.14.
      *
      * Ripple-effect animation
      * Tested and working in: ?IE9+, Chrome (Mobile + Desktop), ?Safari, ?Opera, ?Firefox.
      * JQuery plugin add .ink span in element with class .ripple-effect
      * Animation work on CSS3 by add/remove class .animate to .ink span
-    */
+     */
 
-	(function($) {
-		$('.ripple-effect').click(function(e) {
-			var rippler = $(this);
+    (function ($) {
+        $(".ripple-effect").click(function (e) {
+            var rippler = $(this);
 
-			// create .ink element if it doesn't exist
-			if (rippler.find('.ink').length == 0) {
-				rippler.append("<span class='ink'></span>");
-			}
+            // create .ink element if it doesn't exist
+            if (rippler.find(".ink").length == 0) {
+                rippler.append("<span class='ink'></span>");
+            }
 
-			var ink = rippler.find('.ink');
+            var ink = rippler.find(".ink");
 
-			// prevent quick double clicks
-			ink.removeClass('animate');
+            // prevent quick double clicks
+            ink.removeClass("animate");
 
-			// set .ink diametr
-			if (!ink.height() && !ink.width()) {
-				var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
-				ink.css({ height: d, width: d });
-			}
+            // set .ink diametr
+            if (!ink.height() && !ink.width()) {
+                var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
+                ink.css({ height: d, width: d });
+            }
 
-			// get click coordinates
-			var x = e.pageX - rippler.offset().left - ink.width() / 2;
-			var y = e.pageY - rippler.offset().top - ink.height() / 2;
+            // get click coordinates
+            var x = e.pageX - rippler.offset().left - ink.width() / 2;
+            var y = e.pageY - rippler.offset().top - ink.height() / 2;
 
-			// set .ink position and add class .animate
-			ink
-				.css({
-					top: y + 'px',
-					left: x + 'px'
-				})
-				.addClass('animate');
-		});
-	})(jQuery);
+            // set .ink position and add class .animate
+            ink.css({
+                top: y + "px",
+                left: x + "px",
+            }).addClass("animate");
+        });
+    })(jQuery);
 
-	function internalChecker($) {
-		var internals = $('.link-internal');
-		var url = internals.first().attr('href');
+    function internalChecker($) {
+        var internals = $(".link-internal");
+        var url = internals.first().attr("href");
+        fetchWithTimeout(url, {
+            // method: "GET",
+            timeout: 2000,
+            mode: "no-cors",
+        })
+            .then((res) => {
+                console.log("Status", res.status);
+                if ((res.status < 400 && res.status >= 200) || res.status === 0)
+                    internals.removeClass("tile-disabled");
+                else internals.addClass("tile-disabled");
+            })
+            .catch((err) => {
+                console.log("err", err);
+                internals.addClass("tile-disabled");
+            });
+    }
 
-		fetchWithTimeout(url, {
-			method: 'HEAD',
-			timeout: 2000
-		})
-			.then((res) => {
-				if (res.status < 400 && res.status >= 200) internals.removeClass('tile-disabled');
-				else internals.addClass('tile-disabled');
-			})
-			.catch((err) => {
-				internals.addClass('tile-disabled');
-			});
-	}
-
-	setInterval(internalChecker, 10000, jQuery);
-	internalChecker(jQuery);
+    setInterval(internalChecker, 10000, jQuery);
+    internalChecker(jQuery);
 });
 
 function fetchWithTimeout(resource, options) {
-	var timeout = options.timeout || 8000;
+    var timeout = options.timeout || 8000;
 
-	var controller = new AbortController();
-	var id = setTimeout(() => controller.abort(), timeout);
+    var controller = new AbortController();
+    var id = setTimeout(() => controller.abort(), timeout);
 
-	return fetch(resource, {
-		...options,
-		signal: controller.signal
-	}).then((res) => {
-		clearTimeout(id);
-		return res;
-	});
+    return fetch(resource, {
+        ...options,
+        signal: controller.signal,
+    }).then((res) => {
+        clearTimeout(id);
+        return res;
+    });
 }
